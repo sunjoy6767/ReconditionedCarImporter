43a7964af0b1989cc495a0bd83a38a62499ed820
sunjoy6767 <sonjoyp6767@gmail.com>
2024-12-04 15:51:37 +0600

    Add files via upload

---
+++ Group71_2330031_2330190/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module reconditionedcarimporter.group71_2330031_2330190 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens reconditionedcarimporter.group71_2330031_2330190 to javafx.fxml;
+    exports reconditionedcarimporter.group71_2330031_2330190;
+}
---
+++ Group71_2330031_2330190/src/main/java/reconditionedcarimporter/group71_2330031_2330190/HelloController.java
@@ -0,0 +1,14 @@
+package reconditionedcarimporter.group71_2330031_2330190;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
